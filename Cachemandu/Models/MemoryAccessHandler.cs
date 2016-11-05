554f8704433788495808885970f3de84b37743eb
Ben Williams <w.ben70@gmail.com>
2016-11-05 14:10:02 -0500

    Streamreader in memory access handler

--- Cachemandu/Models/MemoryAccessHandler.cs
+++ Cachemandu/Models/MemoryAccessHandler.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Cachemandu.Models
 {
-    class MemoryAccessHandler
+    public class MemoryAccessHandler
     {
+        StreamReader mLogFile;
+
+        public MemoryAccessHandler(string logFileName)
+        {
+            if (File.Exists(logFileName))
+            {
+                
+            }
+        }
     }
 }
