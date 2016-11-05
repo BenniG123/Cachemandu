61d730df93583423229cf7dd8bd31ffd73a596c0
Ben Williams <w.ben70@gmail.com>
2016-11-05 15:53:09 -0500

    More UI fixes and features

--- Cachemandu/Views/MainPage.xaml.cs
+++ Cachemandu/Views/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,16 +17,39 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Cachemandu
+namespace Cachemandu.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        StorageFile logFile;
+
         public MainPage()
         {
+            logFile = null;
             this.InitializeComponent();
+        }
+
+        private async void PickLogFile(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".txt");
+
+            logFile = await openPicker.PickSingleFileAsync();
+            if (logFile != null)
+            {
+                // Application now has read/write access to the picked file
+                txtLogFileName.Text = logFile.Name;
+            }
+        }
+
+        private void RunSimulation(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Views.SimulationPage));
         }
     }
 }
